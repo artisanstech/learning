ffa29f042b9111e5136305788a365b47081b2e99
artisanstech <67777990+artisanstech@users.noreply.github.com>
2020-07-09 13:30:57 +0530

    Initial Commit

--- app.js
+++ app.js
@@ -1,23 +1,3 @@
-const contractEventNotifier = (store) => (next) => (action) => {
-  if (action.type === EventActions.EVENT_FIRED) {
-    const contract = action.name;
-    const contractEvent = action.event.event;
-    const message = action.event.returnValues._message;
-    const display = `${contract}(${contractEvent}): ${message}`;
-
-    toast.success(display, { position: toast.POSITION.TOP_RIGHT });
-  }
-  return next(action);
-};
-
-const contractEventNotifier = (strore) => (next) => (action) => {
-  if (action.type === EventActions.EVENT_FIRED) {
-    const contract = action.name;
-    const contractEvent = action.event.event;
-    const message = action.event.returnValues._message;
-    const display = `${contract}(${contractEvent}): ${message}`;
-
-    toast.success(display, { position: toast.POSITION.TOP_RIGHT });
-  }
-  return next(action);
-};
+let amount = 123;
+amount = amount.toString();
+showMessage(amount);
