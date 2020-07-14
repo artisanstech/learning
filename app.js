5848908d81158a36f191579e1dc665afd22a6d85
artisanstech <67777990+artisanstech@users.noreply.github.com>
2020-07-14 15:25:42 +0530

    Operators Commit

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
-function continueSample() {
-  for (const item of _products) {
-    if (item.standardCost > 1000) {
-      continue;
-    }
-    console.log(JSON.stringify(item));
+function mathSample() {
+  "use strict";
+
+  let price = 200;
+
+  // And (&&) Operator
+  if (price > 10 && price < 1600) {
+    console.log("price > 10 && price < 1600 is true");
+  }
+  // And (&&) Operator
+  if (price > 10 && price < 200) {
+    console.log("price > 10 && price < 200 is true");
+  } else {
+    console.log("price > 10 && price < 200 is false");
+  }
+
+  if (price > 10 || price < 1600) {
+    console.log("price > 10 || price > 1600 is true");
+  }
+  if (price > 10 || price > 1600) {
+    console.log("price > 10 || price > 1600 is true");
+  }
+
+  if (!(price < 10)) {
+    console.log("!(price < 10) is true");
   }
 }
